0e8efcc0828f0ace60a9ab5fb48740627d087cd2
Sebastian Drela <sebastian.drela@wp.pl>
2024-03-22 01:58:29 +0100

    remove unused namespace

--- Commands/AppliedJobsCommands/ExportExcelAppliedJobsCommand.cs
+++ Commands/AppliedJobsCommands/ExportExcelAppliedJobsCommand.cs
@@ -1,5 +1,4 @@
-﻿using AppliedJobsManager.Models;
-using DocumentFormat.OpenXml;
+﻿using DocumentFormat.OpenXml;
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Spreadsheet;
 using System.Windows.Input;
